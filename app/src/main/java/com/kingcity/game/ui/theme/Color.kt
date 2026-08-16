7814268e4e061e7a03c8139b6e516bcadeaca5ce
llggang03-cmd <llggang03@gmail.com>
2026-08-16 23:19:59 +0530

    Create Color.kt

---
+++ app/src/main/java/com/kingcity/game/ui/theme/Color.kt
@@ -0,0 +1,19 @@
+package com.kingcity.game.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val NightPurpleTop = Color(0xFF0D0B1E)
+val NightPurpleBottom = Color(0xFF2B1B4A)
+val NeonPink = Color(0xFFFF2D95)
+val NeonGold = Color(0xFFFFC72C)
+val WaterBlue = Color(0xFF1565C0)
+val WaterBlueDark = Color(0xFF0D3B73)
+val RoadAsphalt = Color(0xFF2E2A3D)
+val RoadLine = Color(0xFFFFE082)
+val SandTan = Color(0xFF3A2E52)
+
+val TextLight = Color(0xFFF5F3FF)
+val TextDim = Color(0xFFB8AEDC)
+val CardDark = Color(0xFF1E1638)
+val CardDarker = Color(0xFF150F29)
+val WantedRed = Color(0xFFE53935)
